07d624e84a69b0ebb3095c41ef41d1abd6e73a4d
Tony Soto Gómez <tonysotogomez@gmail.com>
2016-04-29 18:49:23 -0500

    Color chrome android

--- application/views/header_view.php
+++ application/views/header_view.php
@@ -3,6 +3,7 @@
   <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="theme-color" content="#00a65a">
     <title>Yliana Deza</title>
     <!-- Tell the browser to be responsive to screen width -->
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
